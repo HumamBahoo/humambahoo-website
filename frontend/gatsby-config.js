3bf2d4998f0e9f7adcda85261f61e760d00c59d1
Humam Bahoo <humam.bahoo@gmail.com>
2023-10-07 12:46:54 -0400

    add gatsby's sanity plugin

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -10,5 +10,13 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+        dataset: process.env.GATSBY_SANITY_DATASET,
+        token: process.env.GATSBY_SANITY_TOKEN,
+      },
+    },
   ],
 };
