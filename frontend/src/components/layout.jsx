1b71db4fa51b7e48fe843d6b6dae5187d4374614
Humam Bahoo <humam.bahoo@gmail.com>
2023-10-07 12:57:16 -0400

    implement layout

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./header";
+import Footer from "./footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
