e4d3557e441221fdb362f4bd2b43e8506feb7cba
Humam Bahoo <humam.bahoo@gmail.com>
2023-10-08 00:14:34 -0400

    implemented homepage

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,10 +1,61 @@
 import React from "react";
+
 import Layout from "../components/layout";
 
-const HomePage = () => {
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { MdOpenInBrowser } from "react-icons/md";
+
+export const query = graphql`
+  query MyQuery {
+    homepage: sanityHomepage {
+      fullName
+      introduction
+      resume {
+        asset {
+          url
+        }
+      }
+      picture {
+        asset {
+          gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+        }
+      }
+    }
+  }
+`;
+
+const HomePage = ({ data }) => {
+  const fullName = data.homepage.fullName;
+  const introduction = data.homepage.introduction;
+  const resumeUrl = data.homepage.resume.asset.url;
+  const personalPhoto = data.homepage.picture.asset.gatsbyImageData;
+
   return (
     <Layout>
-      <div>HomePage</div>
+      <section>
+        <GatsbyImage
+          image={personalPhoto}
+          alt={`${fullName} - Photo`}
+        />
+      </section>
+
+      <section>
+        <h2>{fullName}</h2>
+
+        <div className="about">
+          <h3>Hello...</h3>
+          <p>{introduction}</p>
+        </div>
+
+        <a
+          href={resumeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Resume <MdOpenInBrowser />
+        </a>
+      </section>
     </Layout>
   );
 };
