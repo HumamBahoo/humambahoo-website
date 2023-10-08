407415357ff215d3c503307f77d428fcb6decd56
Humam Bahoo <humam.bahoo@gmail.com>
2023-10-07 23:59:31 -0400

    implement header and footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,7 +1,76 @@
 import React from "react";
 
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { MdEmail } from "react-icons/md";
+import { FaGithubSquare, FaLinkedin, FaYoutubeSquare } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const data = useStaticQuery(graphql`
+    query Footer {
+      site {
+        siteMetadata {
+          siteTitle
+          email
+        }
+      }
+    }
+  `);
+
+  const { siteTitle, email } = data.site.siteMetadata;
+
+  return (
+    <footer>
+      <a href={`mailto:${email}`}>
+        Email Me <MdEmail />
+      </a>
+
+      <div className="quick-links">
+        <h2>Quick Links</h2>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/portfolio">Portfolio</Link>
+        </nav>
+      </div>
+
+      <div className="social-links">
+        <h2>Find Me</h2>
+
+        <nav>
+          <a
+            href="https://www.github.com/humambahoo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
+          <a
+            href="https://www.youtube.com/@humambahoo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutubeSquare />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/humambahoo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </nav>
+
+        <p className="technologies">
+          Built with <span>Gatsby</span> | Styled with <span>Tailwind CSS</span> | Hosted on <span>Netlify</span>
+        </p>
+      </div>
+
+      <p className="copyrights">
+        © {new Date().getFullYear()} <span>{siteTitle}</span>. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <h1>
+          H<span>B</span>
+        </h1>
+      </div>
+
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/portfolio">Portfolio</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
