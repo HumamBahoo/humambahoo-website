89a3dd1a697b94706d9e7bb42ee8143a51f9ae42
Humam Bahoo <humam.bahoo@gmail.com>
2023-10-15 14:43:38 -0400

    refactored and restyled 404page

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import Layout from "../components/layout";
 import { Link } from "gatsby";
 
-import { containerCSS, messageCSS, linkCSS } from "./404.module.scss";
+import { notFoundCSS, wrapperCSS, titleCSS, messageCSS, buttonCSS } from "./404.module.scss";
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <div className={containerCSS}>
-        <div className={messageCSS}>
-          <h2>404: Page Not Found</h2>
-          <p>Sorry, the page you are looking for does not exist.</p>
-        </div>
+      <div className={notFoundCSS}>
+        <div className={wrapperCSS}>
+          <h2 className={titleCSS}>404: Page Not Found</h2>
+          <p className={messageCSS}>Sorry, the page you are looking for does not exist.</p>
 
-        <Link
-          to="/"
-          className={linkCSS}
-        >
-          Home
-        </Link>
+          <Link
+            to="/"
+            className={buttonCSS}
+          >
+            Home
+          </Link>
+        </div>
       </div>
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+export const Head = () => <title>Humam Bahoo - Not Found</title>;
